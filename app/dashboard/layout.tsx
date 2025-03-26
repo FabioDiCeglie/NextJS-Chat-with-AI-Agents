9cdf7ddbabda4341be7d197c05ca7c712e69a55b
FabioDiCeglie <dcfabio96@gmail.com>
2025-03-26 10:13:36 +0100

    feat: navigation provider with react context and consume it with use

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import Header from "@/components/Header";
+import NavigationProvider from "@/lib/NavigationProvider";
+import { Authenticated } from "convex/react";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <NavigationProvider>
+      <div className="flex h-screen">
+        <Authenticated>
+          <h1>Sidebar</h1>
+      </Authenticated>
+
+      <div className="flex-1 bg-red-500">
+        <Header />
+
+        <main>{children}</main>
+      </div>
+    </div>
+    </NavigationProvider>
+  );
+}
